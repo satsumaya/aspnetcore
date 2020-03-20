2e0824e82e0a38aed78494b603706308e8f10dee
Hao Kung <HaoK@users.noreply.github.com>
2020-03-19 19:47:00 -0700

    Update runtests.cmd

--- eng/helix/content/runtests.cmd
+++ eng/helix/content/runtests.cmd
@@ -26,7 +26,7 @@ powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePo
 echo "Checking for Microsoft.AspNetCore.App"
 if EXIST ".\Microsoft.AspNetCore.App" (
     echo "Found Microsoft.AspNetCore.App, copying to %DOTNET_ROOT%\shared\Microsoft.AspNetCore.App\%$runtimeVersion%"
-    xcopy /i /y ".\Microsoft.AspNetCore.App" %DOTNET_ROOT%\shared\Microsoft.AspNetCore.App\%$runtimeVersion%\
+    copy /y .\Microsoft.AspNetCore.App\* %DOTNET_ROOT%\shared\Microsoft.AspNetCore.App\%$runtimeVersion%\
 
     dir %DOTNET_ROOT%\shared\Microsoft.AspNetCore.App\%$runtimeVersion%
 
