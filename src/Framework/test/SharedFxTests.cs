686ae94d989948c31b1ff90812e254200a359140
Hao Kung <HaoK@users.noreply.github.com>
2020-03-23 11:58:26 -0700

    Update SharedFxTests.cs

--- src/Framework/test/SharedFxTests.cs
+++ src/Framework/test/SharedFxTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.AspNetCore.Testing;
 using Newtonsoft.Json.Linq;
 using Xunit;
 using Xunit.Abstractions;
